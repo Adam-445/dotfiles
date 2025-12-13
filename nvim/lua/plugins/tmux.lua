8ad6f3f2e000a3f3f163e94053ec671edcedcdae
Adam-445 <Adamch012016@outlook.com>
2025-12-13 12:01:07 +0100

    fix: Typo in tmux.conf, and configure vim-tmux-navigator for transitioning vim -> tmux

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -2,5 +2,19 @@ return {
   {
     "christoomey/vim-tmux-navigator",
     lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
+      { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+    },
   },
 }
