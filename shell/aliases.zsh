a4934f9637aeb2a43b1c0294abe813c2f9988c68
Adam-445 <Adamch012016@outlook.com>
2025-12-13 13:07:18 +0100

    feat(aliases): add more aliases to aliases.zsh (Docker, Tmux, System, WSL)

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -1,1 +1,16 @@
+
+# Docker
+alias dcu='docker compose down -v && docker compose up -d --build'
+
+# Tmux
+alias ta="tmux attach"
+
+# System
+alias update='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y'
+
+# WSL
+alias clip='clip.exe'
+alias paste='powershell.exe Get-Clipboard'
+
+# Personal scripts
 alias start-project="start-project.sh"
