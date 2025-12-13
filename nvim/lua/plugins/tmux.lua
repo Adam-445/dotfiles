0b6aab54d4a0cc376610080b4458fc9ac9504740
Adam-445 <Adamch012016@outlook.com>
2025-12-13 11:11:18 +0100

    fix: Transitioning between windows in both tmux and vim works now

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -1,9 +1,6 @@
 return {
-  "christoomey/vim-tmux-navigator",
-  keys = {
-    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Go to left window" },
-    { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Go to lower window" },
-    { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Go to upper window" },
-    { "<C-l>", "<cmd>TmuxNaviageRight<cr>", desc = "Go to right window" },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
   },
 }
