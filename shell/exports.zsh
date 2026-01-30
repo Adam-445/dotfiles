45d9f694591fa4c0a10955baf93763bf7caa5c65
Adam-445 <Adamch012016@outlook.com>
2026-01-30 13:11:28 +0100

    chore: delete unnecessary plugins

--- shell/exports.zsh
+++ shell/exports.zsh
@@ -6,7 +6,7 @@ export VISUAL='nvim'
 
 # Path additions
 export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
-export PATH="$HOME/dotfiles/scripts:$PATH"  # Add scripts to PATH!
+export PATH="$HOME/dotfiles/scripts:$PATH"
 export PATH="$PATH:$(go env GOPATH)/bin"
 
 # SDKMAN
