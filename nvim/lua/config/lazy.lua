10576074f50adbbed079b183a3e0cd70f0cb83f5
Adam-445 <Adamch012016@outlook.com>
2026-02-08 11:58:54 +0100

    fix: load edgy before aerial to satisfy layout depndency

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -29,16 +29,16 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.docker" },
 
+    -- UI improvements
+    { import = "lazyvim.plugins.extras.ui.edgy" },
+    { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+
     -- editor enhancements
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.editor.illuminate" },
 
     -- coding tools
     { import = "lazyvim.plugins.extras.coding.yanky" },
-
-    -- UI improvements
-    { import = "lazyvim.plugins.extras.ui.edgy" },
-    { import = "lazyvim.plugins.extras.ui.treesitter-context" },
 
     -- import/override with your plugins
     { import = "plugins" },
